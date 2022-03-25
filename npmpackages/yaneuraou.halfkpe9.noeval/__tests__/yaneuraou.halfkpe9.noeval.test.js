d7c5a1a7c38d361f6638efc878f3cffeecda1857
Mizar <mizar.jp@gmail.com>
2022-03-26 08:43:10 +0900

    - npm work YaneuraOu.wasm

---
+++ npmpackages/yaneuraou.halfkpe9.noeval/__tests__/yaneuraou.halfkpe9.noeval.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const YaneuraOu_HalfKPE9_noeval = require('../lib/yaneuraou.halfkpe9.noeval');
+
+describe('@mizarjp/yaneuraou.halfkpe9.noeval', () => {
+    it('needs tests');
+});
