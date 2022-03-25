2712e302f4f7d3bf5847f52f017683d9006d082f
Mizar <mizar.jp@gmail.com>
2022-03-26 08:43:10 +0900

    - npm work YaneuraOu.wasm

---
+++ npmpackages/yaneuraou.halfkp/__tests__/yaneuraou.halfkp.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const YaneuraOu_HalfKP = require('../lib/yaneuraou.halfkp');
+
+describe('@mizarjp/yaneuraou.halfkp', () => {
+    it('needs tests');
+});
---
+++ npmpackages/yaneuraou.material9/__tests__/yaneuraou.material9.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const YaneuraOu_Material9 = require('../lib/yaneuraou.material9');
+
+describe('@mizarjp/yaneuraou.material9', () => {
+    it('needs tests');
+});
